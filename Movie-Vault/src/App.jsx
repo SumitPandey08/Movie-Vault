e8e33172d45dc481c2e00db69169c66d2487a600
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-12-15 17:38:35 +0530

    React Router setUp

--- Movie-Vault/src/App.jsx
+++ Movie-Vault/src/App.jsx
@@ -1,16 +1,22 @@
-
-import './App.css'
-import NavBar from './components/NavBar'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WatchList from "./components/WatchList";
+import Movie from "./components/Movie";
 
 function App() {
-
-
   return (
     <>
-      
-      <NavBar/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Movie />} />
+
+          <Route path="/watchlist" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Movie-Vault/src/components/Movie.jsx
+++ Movie-Vault/src/components/Movie.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function () {
+function Movie() {
   return (
     <div>
+        <h1>Movie</h1>
       
     </div>
   )
 }
 
-export default 
+export default Movie ; 
