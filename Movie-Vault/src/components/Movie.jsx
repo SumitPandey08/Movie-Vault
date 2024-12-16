6a6759ea7d0432c01d609e7bd88aea2ea0b9cc38
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-12-16 22:46:46 +0530

    APi Connected and APp Starting

--- Movie-Vault/src/components/Movie.jsx
+++ Movie-Vault/src/components/Movie.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import MovieCard from './MovieCard';
+import axios from 'axios'
+import { useEffect , useState} from 'react';
 
 function Movie() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=0be2d109beda11d533b5460dac5d40f8&language=en-US&page=1').then(
+      (response) => {
+        setMovies(response.data.results);
+      }
+    )
+  } , [])
   return (
     <div>
-        <h1>Movie</h1>
+         <div className='text-2xl m-5 font-bold text-center'>
+            Trending Movie
+         </div>
+
+        <div className='flex flex-row justify-between flex-wrap gap-5'>
+            {movies.map((movieDoc) =>{
+              return <MovieCard poster_path={ movieDoc.poster_path } original_title={movieDoc.original_title}/>
+            })}
+
+
+        </div>
       
     </div>
   )
---
+++ Movie-Vault/src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MovieCard({poster_path , original_title}) {
+  return (
+    <div className='h-[45vh] w-[160px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer ' style={{backgroundImage : `url(https://image.tmdb.org/t/p/original/${poster_path})`}}>
+      <div className='text-white text-bold w-full text-center bg-gray-700/60'>{original_title}</div>
+      
+    </div>
+  )
+}
+
+export default MovieCard
